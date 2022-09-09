1dd16992443d2209f8bed51ddc288b9161e03ff6
amerenda <alexm@moove.ai>
2022-09-09 10:36:16 -0600

    vpc peering

--- vpc/main.tf
+++ vpc/main.tf
@@ -18,13 +18,13 @@ module "vpc" {
 }
 
 resource "google_compute_network_peering" "vpc-to-mgmt" {
-  name         = "peering1"
+  name         = "${var.environment}-to-mgmt"
   network      = module.vpc.network_self_link
   peer_network = data.google_compute_network.systems.self_link
 }
 
 resource "google_compute_network_peering" "mgmt-to-vpc" {
-  name         = "peering2"
+  name         = "mgmt-to-${var.environment}"
   network = data.google_compute_network.systems.self_link
   peer_network = module.vpc.network_self_link
 }
