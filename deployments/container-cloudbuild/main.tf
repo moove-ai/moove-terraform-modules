34fc62f7174440c01ae7d7daee2507779d6752e2
amerenda <alexm@moove.ai>
2022-09-16 18:43:33 -0600

    adds container builds for cloudrun

---
+++ deployments/container-cloudbuild/main.tf
@@ -0,0 +1,35 @@
+data "google_service_account" "sa" {
+  project = var.project_id
+  account_id = var.service_account
+}
+
+
+resource "google_cloudbuild_trigger" "trigger" {
+  project = var.project_id
+  location = var.location
+  name = var.build_name
+  description = var.build_description
+  service_account = data.google_service_account.sa.name
+  filename = var.cloudbuild_filename
+  included_files = var.included_files
+  ignored_files = var.ignored_files
+
+  tags = concat(var.tags, [
+    "build",
+  ])
+
+
+  github {
+    owner = "moove-ai"
+    name  = var.git_repo
+
+    push {
+      branch = "^${var.build_branch}$"
+    }
+  }
+
+  substitutions = {
+    _ENVIRONMENT = var.environment
+  }
+
+}
---
+++ deployments/container-cloudbuild/variables.tf
@@ -0,0 +1,65 @@
+variable "project_id" {
+  type = string
+  description = "The project to deploy this build to"
+}
+
+variable "location" {
+  type = string
+  description = "The region this trigger is deployed to"
+  default = "global"
+}
+  
+variable "git_repo" {
+  type = string
+  description = "The Git Repo holding the cloudbuild file"
+}
+
+variable "build_branch" {
+  type = string
+  description = "The build branch to use for this build"
+  default = "main"
+}
+
+variable "environment" {
+  type = string
+  description = "The environment that this build is running in"
+}
+
+variable "cloudbuild_filename" {
+  type = string
+  description = "The path and name of the cloudbuild file"
+  default = "cloudbuild.yaml"
+}
+  
+variable "build_name" {
+  type = string
+  description = "The name of the build"
+}
+
+variable "build_description" {
+  type = string
+  description = "The description."
+}
+
+variable "service_account" {
+  type = string
+  description = "The service account running the build"
+}
+
+variable "tags" {
+  type = list(string)
+  description = "List of tags to add to the build"
+  default = []
+}
+
+variable "included_files" {
+  type = list(string)
+  description = "List of files which will trigger the build if changed"
+  
+}
+
+variable "ignored_files" {
+  type = list(string)
+  description = "List of files to exclude for the build trigger."
+  default = []
+}
