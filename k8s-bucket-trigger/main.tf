05c832e942f1c4ec0d6ddc365fe8f25ae4719d3e
amerenda <alexm@moove.ai>
2022-11-09 12:47:55 -0700

    updates service account naming

--- k8s-bucket-trigger/main.tf
+++ k8s-bucket-trigger/main.tf
@@ -9,8 +9,8 @@ locals {
 resource "google_service_account" "service-account" {
   project      = var.project_id
   account_id   = var.service_account_id
-  display_name = "Transform Wejo JTR"
-  description  = "Service account used for the transform-wejo-jtr-for-str application"
+  display_name = "${title(replace(var.app_name, "-", " "))} (K8s)"
+  description  = "Used for the ${var.app_name} application. Coresponding K8s sa: ${var.k8s_sa} in namespace: ${var.namespace}" 
 }
 
 resource "google_service_account_iam_member" "workload-identity" {
