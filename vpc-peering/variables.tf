69b3002a40a3618c1450b09026177144c16cd51f
amerenda <alexm@moove.ai>
2022-11-09 13:51:04 -0700

    adds vpc peering module

---
+++ vpc-peering/variables.tf
@@ -0,0 +1,19 @@
+variable "network_name" {
+  type = string
+  description = "The name of the network running in this environemt"
+}
+
+variable "network_project_id" {
+  type        = string
+  description = "The project the local network is being run in"
+}
+
+variable "remote_network_name" {
+  type = string
+  description = "The name of the network to peer to"
+}
+
+variable "remote_network_project_id" {
+  type        = string
+  description = "The project the remote network is being run in"
+}
