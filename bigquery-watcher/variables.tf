d272e4f537ef4d201a90a2d72c5597814d6a133f
amerenda <alexm@moove.ai>
2022-11-13 10:21:31 -0700

    changes default bq watcher sa

--- bigquery-watcher/variables.tf
+++ bigquery-watcher/variables.tf
@@ -6,7 +6,7 @@ variable "project_id" {
 variable "service_account_id" {
   type        = string
   description = "The Account ID of the SA to create"
-  default     = "bigquery-watcher"
+  default     = "k8s-bigquery-watcher"
 }
 
 variable "service_account_description" {
