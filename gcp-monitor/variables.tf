73721a6aad6cea78fdb8e9f5add321bed7a025b7
Alex Merenda <alexm@moove.ai>
2022-11-25 09:25:33 -0800

    monitor all projects in folder

--- gcp-monitor/variables.tf
+++ gcp-monitor/variables.tf
@@ -1,8 +1,3 @@
-variable "monitored_projects" {
-  type        = list(string)
-  description = "A list of projects to include in the monitoring scope"
-}
-
 variable "metrics_scope" {
   type        = string
   description = "The project that holds the metrics scopes"
@@ -17,4 +12,9 @@ variable "secret_project_id" {
   type        = string
   description = "The project holding GCP secrets"
   default     = "moove-secrets"
+}
+
+variable "folder_id" {
+  type =  string
+  description = "The folder ID to monitor. Will get all projets in this folder. This module will need to be run again when a new project is added"
 }
