242444da5aa6f4539b77d8d26cb6c98976cfdea3
amerenda <alexm@moove.ai>
2022-12-01 07:48:46 -0700

    module: jupyter

--- jupyter/main.tf
+++ jupyter/main.tf
@@ -1,3 +1,15 @@
+/**
+ * # Jupyter
+ *
+ * This module sets up resources around Jupyter and JupyterHub
+ * You must assign a GCP admin role to the serviceaccount running this terraform.
+ * Details here: [Assigning an admin role to the serviceaccount](https://cloud.google.com/identity/docs/how-to/setup#assigning_an_admin_role_to_the_service_account)
+ * 
+ *
+ * Written by Alex Merenda for moove.ai
+ */
+
+
 resource "google_service_account" "sa" {
   project      = var.project_id
   account_id   = var.service_account_id
