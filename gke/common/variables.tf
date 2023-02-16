f0a4fe1d2c3fc35944d8da6d79355b4fa1ba3c10
amerenda <alexm@moove.ai>
2023-02-16 16:19:38 -0700

    updates variables

--- gke/common/variables.tf
+++ gke/common/variables.tf
@@ -7,3 +7,8 @@ variable cluster_network_project_id {
   type        = string
   description = "The project the cluster network is located in"
 }
+
+variable cluster_network {
+  type        = string
+  description = "The name of the VPC for this environment"
+}
