ec0813e9dd9a8bee6aed0035f03604db81447011
amerenda <alexm@moove.ai>
2023-02-17 08:50:17 -0700

    updates variables

--- gke/cluster/outputs.tf
+++ gke/cluster/outputs.tf
@@ -1,7 +1,3 @@
 output "cluster_name" {
   value = "${var.environment}-${var.region}"
 }
-
-output "proxy_dns_name" {
-  value = "${google_dns_record_set.proxy.name}.${data.google_dns_managed_zone.moove-internal.name}"
-}
