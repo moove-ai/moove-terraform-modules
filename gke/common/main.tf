974d6f4db5943c9e8832c4d8b233c02bf07cf2f5
amerenda <alexm@moove.ai>
2023-02-17 08:55:22 -0700

    updates roles

--- gke/common/main.tf
+++ gke/common/main.tf
@@ -65,3 +65,13 @@ resource "google_project_iam_member" "network-container-service-agent" {
   role    = "roles/containerregistry.ServiceAgent"
   member  = "serviceAccount:${google_service_account.k8s-nodes.email}"
 }
+
+resource "google_project_iam_member" "network-host-service-agent" {
+  project = var.cluster_network_project_id
+  role    = "roles/container.hostServiceAgentUser"
+  member  = "serviceAccount:service-${data.google_project.project.number}@container-engine-robot.iam.gserviceaccount.com"
+}
+
+data "google_project" "project" {
+  project_id = var.project_id
+}
