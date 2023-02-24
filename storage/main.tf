53976a49f8749bdb6d6bd3da33c1aad811e31171
amerenda <alexm@moove.ai>
2023-02-24 11:26:44 -0700

    adding storage trigger (generic)

---
+++ storage/main.tf
@@ -0,0 +1,148 @@
+/**
+ * # GCS Storage Modules 
+ *
+ * Creates resources around any internal app which should be triggered on the creation of a GCS object
+ *
+ * 
+ * Written by Alex Merenda for moove.ai
+ */
+
+locals {
+  service_account_project_id = var.service_account_project_id != "" ? var.service_account_project_id : var.project_id
+}
+
+
+# Application
+resource "google_service_account" "service-account" {
+  count        = var.create_service_account ? 1 : 0
+  project      = local.service_account_project_id
+  account_id   = var.service_account_id
+  display_name = var.name
+  description  = var.service_account_description
+}
+
+data "google_service_account" "service-account" {
+  account_id = var.service_account_id
+  project    = local.service_account_project_id
+}
+
+
+data "google_storage_project_service_account" "gcs_account" {
+  project = var.project_id
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = var.name
+  project  = var.project_id
+  location = var.location
+  labels = merge({
+    project     = var.project_id
+    terraformed = "true",
+    environment = var.environment,
+  }, var.labels)
+
+  dynamic "lifecycle_rule" {
+    for_each = [for rule in var.lifecycle_rules : {
+      action_type          = rule.action.type
+      action_storage_class = lookup(rule.action, "storage_class", null)
+
+      condition_age                   = lookup(rule.condition, "age", null)
+      condition_created_before        = lookup(rule.condition, "created_before", null)
+      condition_with_state            = lookup(rule.condition, "with_state", null)
+      condition_matches_storage_class = lookup(rule.condition, "matches_storage_class", null)
+      condition_num_newer_versions    = lookup(rule.condition, "num_newer_versions", null)
+    }]
+
+    content {
+      action {
+        type          = lifecycle_rule.value.action_type
+        storage_class = lifecycle_rule.value.action_storage_class
+      }
+      condition {
+        age                   = lifecycle_rule.value.condition_age
+        created_before        = lifecycle_rule.value.condition_created_before
+        with_state            = lifecycle_rule.value.condition_with_state
+        matches_storage_class = lifecycle_rule.value.condition_matches_storage_class
+        num_newer_versions    = lifecycle_rule.value.condition_num_newer_versions
+      }
+    }
+  }
+}
+
+data "google_storage_bucket" "input-bucket" {
+  count = var.create_bucket ? 0 : 1
+  name  = var.name
+}
+
+resource "google_storage_notification" "bucket-notification" {
+  count          = var.notification_enabled ? 1 : 0
+  bucket         = var.name
+  payload_format = "JSON_API_V1"
+  topic          = google_pubsub_topic.topic[0].id
+  event_types    = ["OBJECT_FINALIZE"]
+  depends_on = [
+    google_pubsub_topic_iam_binding.binding,
+  ]
+}
+
+# Pub/Sub 
+resource "google_pubsub_topic" "topic" {
+  count   = var.notification_enabled ? 1 : 0
+  project = var.project_id
+  name    = var.topic_name == "" ? var.name : var.topic_name
+  labels = merge({
+    terraformed = "true",
+    environment = var.environment,
+  }, var.labels)
+}
+
+resource "google_pubsub_topic_iam_binding" "binding" {
+  count   = var.notification_enabled ? 1 : 0
+  project = google_pubsub_topic.topic[0].project
+  topic   = google_pubsub_topic.topic[0].id
+  role    = "roles/pubsub.publisher"
+  members = ["serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"]
+  depends_on = [
+    data.google_storage_project_service_account.gcs_account,
+  ]
+}
+
+resource "google_pubsub_subscription" "subscription" {
+  count                = var.notification_enabled ? 1 : 0
+  name                 = var.subscription_name == "" ? var.name : var.subscription_name
+  project              = google_pubsub_topic.topic[0].project
+  topic                = google_pubsub_topic.topic[0].id
+  ack_deadline_seconds = var.ack_deadline_seconds
+  labels = merge({
+    terraformed = "true",
+    environment = var.environment,
+  }, var.labels)
+}
+
+resource "google_pubsub_subscription_iam_member" "subscriber" {
+  count        = var.notification_enabled ? 1 : 0
+  subscription = google_pubsub_subscription.subscription[0].name
+  project      = google_pubsub_subscription.subscription[0].project
+  role         = "roles/pubsub.subscriber"
+  member       = "serviceAccount:${var.create_service_account == true ? google_service_account.service-account[0].email : data.google_service_account.service-account.email}"
+}
+
+resource "google_storage_bucket_iam_member" "admin" {
+  for_each = toset(var.admin_members)
+  bucket   = google_storage_bucket.bucket.name
+  role     = "roles/storage.objectAdmin"
+  member   = each.key
+}
+
+resource "google_storage_bucket_iam_member" "read" {
+  for_each = toset(var.read_members)
+  bucket   = google_storage_bucket.bucket.name
+  role     = "roles/storage.objectViewer"
+  member   = each.key
+}
+
+resource "google_storage_bucket_iam_member" "sa-admin" {
+  bucket = google_storage_bucket.bucket.name
+  role   = "roles/storage.objectAdmin"
+  member       = "serviceAccount:${var.create_service_account == true ? google_service_account.service-account[0].email : data.google_service_account.service-account.email}"
+}
---
+++ storage/variables.tf
@@ -0,0 +1,98 @@
+variable "name" {
+  type        = string
+  description = "The name of the bucket to create"
+}
+
+variable "project_id" {
+  type        = string
+  description = "The project this bucket is located in"
+}
+
+variable "location" {
+  type        = string
+  default     = "US"
+  description = "What GCP location should this bucket be stored in"
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment using this bucket"
+}
+
+variable "labels" {
+  type        = map(string)
+  default     = {}
+  description = "Map of labels to apply to this bucket"
+}
+
+variable "lifecycle_rules" {
+  description = "The lifecycle rules to be applied to the input bucket. If this array is populated then each element in it will be applied as a lifecycle rule to this bucket. The structure of each element is described in detail here: https://www.terraform.io/docs/providers/google/r/storage_bucket.html#lifecycle_rule. See also: https://cloud.google.com/storage/docs/lifecycle#configuration."
+  default     = []
+}
+
+variable "admin_members" {
+  type        = list(string)
+  default     = []
+  description = "List of members to give storage object admin access to. eg: user:jane@example.com"
+}
+
+variable "read_members" {
+  type        = list(string)
+  default     = []
+  description = "List of members to give storage object read only access to. eg: user:jane@example.com"
+}
+
+variable "create_bucket" {
+  type        = bool
+  default     = true
+  description = "Set to false to use an existing bucket"
+}
+
+variable "create_service_account" {
+  type        = bool
+  default     = true
+  description = "Set to false to use an existing service account"
+}
+
+variable "notification_enabled" {
+  type        = bool
+  description = "Set to false to disable the bucket notification. Useful for testing."
+  default     = false
+}
+
+variable "service_account_project_id" {
+  type        = string
+  default     = ""
+  description = "Optional. Sets the project for an existing service account"
+}
+
+variable "service_account_id" {
+  type        = string
+  description = "The name of the service account. Either uses an existing account or creates a new one."
+}
+
+variable service_account_description {
+  type        = string
+  default     = ""
+  description = "The description for the service account if creating"
+}
+
+
+
+variable "topic_name" {
+  type        = string
+  default     = ""
+  description = "Optional. Sets the name of the GCS storage notification topic. Only used if notification_enabled is true"
+}
+
+variable "subscription_name" {
+  type        = string
+  default     = ""
+  description = "Optional. Sets the name of the GCS storage notification subscription. Only used if notification_enabled is true"
+}
+
+variable "ack_deadline_seconds" {
+  type        = string
+  default     = "60"
+  description = "Optional. Sets the ack deadline for this pub/sub subscription"
+}
