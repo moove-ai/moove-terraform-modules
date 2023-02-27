fc634fdc6c9b2b14fafb3be9a58e3ed7132a1095
Alex Merenda <alexm@moove.ai>
2023-02-27 10:07:16 -0700

    Production context (#54)
    * adding storage trigger (generic)
    
    * adding service account module
    
    * adding cloud scheduler
    
    * adding cloud scheduler
    
    * adds option to create pub/sub resources

---
+++ cloud-scheduler/main.tf
@@ -0,0 +1,21 @@
+
+resource "google_cloud_scheduler_job" "job" {
+  name             = var.job_name
+  description      = var.job_description
+  schedule         = var.job_schedule
+  time_zone        = var.job_timezone
+  attempt_deadline = var.job_deadline
+
+  retry_config {
+    retry_count = var.job_retry_count
+  }
+
+  http_target {
+    http_method = var.http_method
+    uri         = var.http_uri
+  }
+
+  oauth_token {
+    service_account_email = data.google_compute_default_service_account.default.email 
+  }
+}
