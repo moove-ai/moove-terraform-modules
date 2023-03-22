d44d6dd3c0ea81a647efcdbab0853744f6464f80
amerenda <alexm@moove.ai>
2023-03-22 11:54:22 -0600

    updates storage module for clients

--- storage/client.tf
+++ storage/client.tf
@@ -24,14 +24,14 @@ resource "google_secret_manager_secret" "client-id" {
 
 resource "google_secret_manager_secret_version" "client-id-version" {
   count       = var.client_bucket ? 1 : 0
-  secret      = google_secret_manager_secret.client-id.id
+  secret      = google_secret_manager_secret.client-id[0].id
   secret_data = var.client_id
 }
 
 
 resource "google_storage_hmac_key" "key" {
   count                 = var.client_hmac_key ? 1 : 0
-  service_account_email = google_service_account.sa.email
+  service_account_email = google_service_account.sa[0].email
 }
 
 resource "google_secret_manager_secret" "client-hmac-key" {
@@ -52,13 +52,35 @@ resource "google_secret_manager_secret" "client-hmac-key" {
 
 resource "google_secret_manager_secret_version" "client-key-version" {
   count       = var.client_hmac_key ? 1 : 0
-  secret      = google_secret_manager_secret.client-hmac-key.id
+  secret      = google_secret_manager_secret.client-hmac-key[0].id
   secret_data = local.client-key
 }
 
 locals {
   client-key = <<-EOT
-    ACCESS_KEY=${var.client_hmac_key ? google_storage_hmac_key.key.access_id : "null"}
-    SECRET_ACCESS_KEY=${var.client_hmac_key ? google_storage_hmac_key.key.secret : "null"}
+    ACCESS_KEY=${var.client_hmac_key ? google_storage_hmac_key.key[0].access_id : "null"}
+    SECRET_ACCESS_KEY=${var.client_hmac_key ? google_storage_hmac_key[0].key.secret : "null"}
     EOT
+}
+
+resource "google_secret_manager_secret" "client-sa-key" {
+  count     = var.client_sa_key ? 1 : 0
+  project   = var.secret_project
+  secret_id = "client-key_${var.client_name}"
+
+  labels = {
+    terraformed = "true"
+    secret-data = "terraform"
+    client      = var.client_name
+  }
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "client-sa-key-version" {
+  count       = var.client_hmac_key ? 1 : 0
+  secret      = google_secret_manager_secret.client-sa-key[0].id
+  secret_data = local.client-key
 }
