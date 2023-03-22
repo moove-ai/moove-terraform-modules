0dd52ee221c9abf7843eedd946e28d6779af736c
amerenda <alexm@moove.ai>
2023-03-22 12:26:44 -0600

    updates storage module for clients

--- storage/client.tf
+++ storage/client.tf
@@ -1,11 +1,3 @@
-resource "google_service_account" "client" {
-  count        = var.client_bucket ? 1 : 0
-  project      = var.project_id
-  account_id   = "moove-data-import-${var.client_id}-${lower(var.location)}"
-  display_name = "${var.client_name} data import"
-  description  = "Service account used for data import for ${var.client_name} (${var.client_id})"
-}
-
 resource "google_secret_manager_secret" "client-id" {
   count     = var.client_bucket ? 1 : 0
   project   = var.secret_project
@@ -31,7 +23,7 @@ resource "google_secret_manager_secret_version" "client-id-version" {
 
 resource "google_storage_hmac_key" "key" {
   count                 = var.client_hmac_key ? 1 : 0
-  service_account_email = google_service_account.client[0].email
+  service_account_email = var.create_service_account ? google_service_account.service-account.email : data.google_service_account.service-account.email
 }
 
 resource "google_secret_manager_secret" "client-hmac-key" {
@@ -49,38 +41,3 @@ resource "google_secret_manager_secret" "client-hmac-key" {
     automatic = true
   }
 }
-
-#resource "google_secret_manager_secret_version" "client-key-version" {
-#  count       = var.client_hmac_key ? 1 : 0
-#  secret      = google_secret_manager_secret.client-hmac-key[0].id
-#  secret_data = local.client-key
-#}
-
-#locals {
-#  client-key = <<-EOT
-#    ACCESS_KEY=${var.client_hmac_key ? google_storage_hmac_key.key[0].access_id : "null"}
-#    SECRET_ACCESS_KEY=${var.client_hmac_key ? google_storage_hmac_key[0].key.secret : "null"}
-#    EOT
-#}
-
-resource "google_secret_manager_secret" "client-sa-key" {
-  count     = var.client_sa_key ? 1 : 0
-  project   = var.secret_project
-  secret_id = "client-key_${var.client_name}"
-
-  labels = {
-    terraformed = "true"
-    secret-data = "terraform"
-    client      = var.client_name
-  }
-
-  replication {
-    automatic = true
-  }
-}
-
-#resource "google_secret_manager_secret_version" "client-sa-key-version" {
-#  count       = var.client_hmac_key ? 1 : 0
-#  secret      = google_secret_manager_secret.client-sa-key[0].id
-#  secret_data = local.client-key
-#}
