294b4ff77b43f13b3f16b1473b810a1b7628badf
amerenda <alexm@moove.ai>
2023-03-22 12:27:03 -0600

    updates storage module for clients

--- storage/client.tf
+++ storage/client.tf
@@ -23,7 +23,7 @@ resource "google_secret_manager_secret_version" "client-id-version" {
 
 resource "google_storage_hmac_key" "key" {
   count                 = var.client_hmac_key ? 1 : 0
-  service_account_email = var.create_service_account ? google_service_account.service-account.email : data.google_service_account.service-account.email
+  service_account_email = var.create_service_account ? google_service_account.service-account[0].email : data.google_service_account.service-account[0].email
 }
 
 resource "google_secret_manager_secret" "client-hmac-key" {
