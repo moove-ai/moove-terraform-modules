7662b71cfdd5e06246a18200148468b1ba554f22
amerenda <alexm@moove.ai>
2023-06-27 13:09:42 -0600

    cloud cloud

--- service-account/output.tf
+++ service-account/output.tf
@@ -11,7 +11,13 @@ output "email" {
 }
 
 output "project_id" {
-  value       = google_service_account.service-account.account_id
+  value       = google_service_account.service-account.project_id
   sensitive   = false
   description = "The project id of the service accoutn"
+}
+
+output "id" {
+  value       = google_service_account.service-account.id
+  sensitive   = false
+  description = "The account ID of the service accoutn"
 }
