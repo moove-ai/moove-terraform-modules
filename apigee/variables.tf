0304e92e5987e12134b85e68b56c42f87c91a393
amerenda <alexm@moove.ai>
2023-06-28 08:50:21 -0600

    adds apigee

---
+++ apigee/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {}
+variable "apigee_range_name" {}
+variable "apigee_range_prefix_length" {}
+variable "apigee_network_id" {}
+
+variable "secret_name" {}
+variable "secret_project_id" {}
+variable "secret_region" {}
+variable "analytics_region" {}
+variable "apigee_name" {}
+variable "apigee_description" {}
