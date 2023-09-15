12966edf445215c05ac1551acc9f20285fb09c6a
Alex Merenda <alexm@moove.ai>
2023-09-15 09:53:12 -0600

    fixes output for cloudfunctions (#64)

--- deployments/cloudfunctions/output.tf
+++ deployments/cloudfunctions/output.tf
@@ -1,14 +1,12 @@
 output "trigger_url" {
-  count       = var.function_trigger_topic == "" ? 1 : 0
-  value       = "https://${var.region}-${var.function_project_id}.cloudfunctions.net/${var.function_name}"
+  value       = var.function_trigger_topic != "" ? "https://${var.function_region}-${var.function_project_id}.cloudfunctions.net/${var.function_name}" : null
   sensitive   = false
   description = "The URL of the CF"
   depends_on  = []
 }
 
 output "trigger_topic" {
-  count       = var.function_trigger_topic == "" ? 0 : 1
-  value       = var.function_trigger_topic
+  value       = var.function_trigger_topic != "" ? var.function_trigger_topic : null
   sensitive   = false
   description = "The topic which triggers the CF"
   depends_on  = []
