00be8f916f15f7228cc425ad923026e35c5005b5
amerenda <alexm@moove.ai>
2023-09-15 09:58:36 -0600

    adds resource monitor query

--- gke/cluster/main.tf
+++ gke/cluster/main.tf
@@ -16,26 +16,3 @@ data "google_service_account" "k8s-nodes" {
   account_id = "k8s-nodes"
   project    = var.project_id
 }
-
-
-resource "google_bigquery_dataset" "resource-monitor" {
-  count         = var.create_resource_monitor ? 1 : 0
-  project       = var.project_id
-  dataset_id    = "${local._cluster_name}_export"
-  friendly_name = "Kubernetes Usage Export for ${var.cluster_name}"
-  location      = "US"
-  labels = {
-    "environment" = var.environment
-    "cluster"     = var.cluster_name
-  }
-
-  access {
-    role          = "OWNER"
-    user_by_email = data.google_service_account.k8s-nodes.email
-  }
-
-  access {
-    role   = "READER"
-    domain = "moove.ai"
-  }
-}
