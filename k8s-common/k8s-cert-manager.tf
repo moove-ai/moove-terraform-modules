34946eeb62be9222c81058b5e708527c14b4671a
amerenda <alexm@moove.ai>
2023-09-15 10:31:45 -0600

    t

--- k8s-common/k8s-cert-manager.tf
+++
@@ -1,6 +0,0 @@
-resource "google_service_account_iam_member" "cert-manager-workload-identity" {
-  count              = var.install_cert_manager ? 1 : 0
-  member             = "serviceAccount:${var.project_id}.svc.id.goog[default/cert-manager]"
-  role               = "roles/iam.workloadIdentityUser"
-  service_account_id = data.google_service_account.dns-admin[0].name
-}
