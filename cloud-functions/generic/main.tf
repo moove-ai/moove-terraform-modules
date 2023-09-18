eacec11867c5c269a447c6d3b6fe4fb548266fcd
amerenda <alexm@moove.ai>
2023-09-18 11:52:38 -0600

    updates deployer to have permission to deploy to cloud functions

--- cloud-functions/generic/main.tf
+++ cloud-functions/generic/main.tf
@@ -1,38 +1,71 @@
 locals {
-  build_name           = var.build_name != "" ? var.build_name : "build-${var.github_repo}"
-  build_included_files = var.build_included_files != [] ? var.build_included_files : ["**"]
-  build_ignored_files  = var.build_ignored_files != [] ? var.build_ignored_files : ["helm/**", "deploy.yaml", "stage.yaml"]
-  build_tags           = [var.github_repo, "build"]
+  build_included_files = var.build_included_files != [] ? var.build_included_files : ["cloud-functions/${var.function_name}/**"]
+  build_file           = var.build_file != "" ? var.build_file : "cloud-functions/${var.function_name}/cloudbuild.yaml"
+  build_tags           = [var.github_repo, "deplpy", "cloud-function"]
+  roles_list           = concat(["roles/cloudfunctions.invoker"], var.function_additional_roles)
+
+  env_role_combinations = flatten([
+    for env_key, env_value in var.environments : [
+      for role in local.roles_list : {
+        env_key    = env_key
+        project_id = env_value.project_id
+        role       = role
+      }
+    ]
+  ])
 }
 
-resource "google_cloudbuild_trigger" "build" {
-  name            = local.build_name
-  location        = var.location
-  project         = var.project_id
-  service_account = "projects/${var.project_id}/serviceAccounts/deployer@${var.project_id}.iam.gserviceaccount.com"
-  included_files  = local.build_included_files
-  ignored_files   = local.build_ignored_files
-  tags            = local.build_tags
+data "google_service_account" "scoring" {
+  for_each   = var.environments
+  project    = each.value.project_id
+  account_id = var.service_account_id
+}
 
-  filename = var.build_file
+data "google_service_account" "deployer" {
+  project    = var.build_project_id
+  account_id = var.build_service_account_id
+}
+
+resource "google_project_iam_member" "scoring-iam" {
+  for_each = { for combo in local.env_role_combinations : "${combo.env_key}-${combo.role}" => combo }
+  project  = each.value.project_id
+  role     = each.value.role
+  member   = "serviceAccount:${data.google_service_account.scoring[each.value.env_key].email}"
+}
+
+resource "google_service_account_iam_member" "deployer-act-as" {
+  for_each           = var.environments
+  service_account_id = data.google_service_account.scoring[each.key].id
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:${data.google_service_account.deployer.email}"
+}
+
+resource "google_cloudbuild_trigger" "deploy" {
+  for_each        = var.environments
+  name            = "deploy-cf-${var.function_name}-${each.key}"
+  location        = var.build_location
+  project         = var.build_project_id
+  service_account = data.google_service_account.deployer.id
+  included_files  = local.build_included_files
+  ignored_files   = var.build_ignored_files
+  tags            = concat(var.build_tags, local.build_tags, [each.key, each.value.region, each.value.cloud_function_function])
+  description     = "Deploy the ${var.function_name} function to ${each.key} ${each.value.region}"
+
+  filename = local.build_file
+
+  substitutions = merge({
+    _FUNCTION_NAME            = var.function_name
+    _FUNCTION_REGION          = each.value.region
+    _FUNCTION_ENVIRONMENT     = each.key
+    _FUNCTION_PROJECT_ID      = each.value.project_id
+    _FUNCTION_SERVICE_ACCOUNT = data.google_service_account.scoring[each.key].email
+  }, var.build_additional_substitutions)
 
   github {
     owner = "moove-ai"
     name  = var.github_repo
     push {
-      branch = var.build_branch_pattern
+      branch = each.value.build_branch_pattern
     }
   }
 }
-
-data "google_service_account" "deployer" {
-  project    = var.deployer_project_id
-  account_id = var.deployer_account_id
-}
-
-resource "google_project_iam_member" "deployer-storage" {
-  project = var.project_id
-  role    = "roles/storage.objectAdmin"
-  member  = "serviceAccount:${data.google_service_account.deployer.email}"
-}
- 
