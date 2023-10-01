92360c73c7a2cf70603d6c82ac986c3cdbb7af60
amerenda <alexm@moove.ai>
2023-10-01 15:25:56 -0600

    adds generic cloudbuild module

---
+++ ci-cd/cloud-build/main.tf
@@ -0,0 +1,20 @@
+resource "google_cloudbuild_trigger" "build" {
+  name            = var.build_name
+  location        = "global"
+  project         = var.project_id
+  service_account = "projects/${var.project_id}/serviceAccounts/deployer@${var.project_id}.iam.gserviceaccount.com"
+  included_files  = var.build_included_files
+  ignored_files   = var.build_ignored_files
+  tags            = var.build_tags
+  disabled        = var.disable_trigger
+
+  filename = var.build_file
+
+  github {
+    owner = "moove-ai"
+    name  = var.github_repo
+    push {
+      branch = var.build_branch_pattern
+    }
+  }
+}
---
+++ ci-cd/cloud-build/variables.tf
@@ -0,0 +1,49 @@
+variable "build_name" {
+  description = "Name of the Cloud Build trigger."
+  type        = string
+}
+
+variable "project_id" {
+  description = "ID of the Google Cloud project."
+  default     = "moove-build"
+  type        = string
+}
+
+variable "build_included_files" {
+  description = "List of files to include in the build."
+  type        = list(string)
+}
+
+variable "build_ignored_files" {
+  description = "List of files to ignore in the build."
+  type        = list(string)
+  default     = []
+}
+
+variable "build_tags" {
+  description = "List of tags to associate with the build trigger."
+  type        = list(string)
+  default     = []
+}
+
+variable "disable_trigger" {
+  description = "Whether to disable the trigger (true or false)."
+  type        = bool
+  default     = false
+}
+
+variable "build_file" {
+  description = "Name of the build configuration file (e.g., cloudbuild.yaml)."
+  type        = string
+  default     = "cloudbuild.yaml"
+}
+
+variable "github_repo" {
+  description = "Name of the GitHub repository."
+  type        = string
+}
+
+variable "build_branch_pattern" {
+  description = "Branch pattern to trigger the build on GitHub pushes."
+  type        = string
+}
