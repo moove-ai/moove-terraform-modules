5f05edda70a4ee522f74e1da4221bc376ed2f1ea
amerenda <alexm@moove.ai>
2023-10-10 11:37:35 -0400

    fixes merge conflicts

--- gcp-monitor/main.tf
+++ gcp-monitor/main.tf
@@ -7,24 +7,8 @@
  * 
  * Written by Alex Merenda for moove.ai
  */
-data "google_projects" "projects" {
-  filter = "parent.id:${var.folder_id}"
-}
-
-
-data "google_project" "project" {
-  count      = length(data.google_projects.projects.projects[*].project_id)
-  project_id = data.google_projects.projects.projects[count.index].project_id
-}
-
 locals {
-  projects_map = { 
-    for project in data.google_projects.projects.projects[*] : 
-    project.project_id => project.project_id 
-    if !contains(values(project), var.metrics_scope) && 
-       !contains(var.ignored_projects, project.project_id) &&
-       project.lifecycle_state == "ACTIVE"
-  }
+  projects_map = { for id in var.monitored_projects : id => id }
 }
 
 resource "google_monitoring_monitored_project" "project" {
@@ -70,7 +54,6 @@ resource "google_service_account_key" "monitor-key" {
   service_account_id = google_service_account.monitor.name
   public_key_type    = "TYPE_X509_PEM_FILE"
 }
-
 
 resource "google_secret_manager_secret" "monitor-sa" {
   project   = var.secret_project_id
--- gcp-monitor/variables.tf
+++ gcp-monitor/variables.tf
@@ -14,13 +14,7 @@ variable "secret_project_id" {
   default     = "moove-secrets"
 }
 
-variable "folder_id" {
-  type        = string
-  description = "The folder ID to monitor. Will get all projets in this folder. This module will need to be run again when a new project is added"
-}
-
-variable "ignored_projects" {
-  type = list(string)
-  description = "List of projects to ignore when attaching monitoring scopes"
-  default = []
+variable "monitored_projects" {
+  description = "List of project IDs to monitor"
+  type        = list(string)
 }
