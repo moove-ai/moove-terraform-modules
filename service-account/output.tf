5fdb3415fcc40d988f5f1091eef11386eaf5b4a4
amerenda <alexm@moove.ai>
2023-11-14 10:37:05 -0500

    adds prometheus bigquery exporter module

--- service-account/output.tf
+++ service-account/output.tf
@@ -1,17 +1,23 @@
 output "account_id" {
   value       = google_service_account.service-account.account_id
   sensitive   = false
-  description = "The account ID of the service accoutn"
+  description = "The account ID of the service account"
 }
 
 output "email" {
   value       = google_service_account.service-account.email
   sensitive   = false
-  description = "The account ID of the service accoutn"
+  description = "The account ID of the service account"
 }
 
 output "id" {
   value       = google_service_account.service-account.id
   sensitive   = false
-  description = "The account ID of the service accoutn"
+  description = "The account ID of the service account"
+}
+
+output "name" {
+  value       = google_service_account.service-account.name
+  sensitive   = false
+  description = "The name of the service account"
 }
