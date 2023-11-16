e04f3abdf97467e88a5135db026e1cd58c87e117
amerenda <alexm@moove.ai>
2023-11-16 08:50:17 -0500

    using GKE

---
+++ applications/blue-owl-transfer/main.tf
@@ -0,0 +1,186 @@
+locals {
+  service_account_project_id   = var.service_account_project_id != "" ? var.service_account_project_id : var.project_id
+  service_account_display_name = var.service_account_display_name != "" ? var.service_account_display_name : var.service_account_id
+  service_account_description  = var.service_account_description != "" ? var.service_account_description : var.service_account_display_name
+}
+
+resource "google_secret_manager_secret" "gcs-access-key" {
+  project   = var.project_id
+  secret_id = "blue-owl-transfer-gcs-access-key"
+
+  labels = {
+    environment = var.environment
+    function    = "blue-owl-transfer"
+    client      = "blue-owl"
+  }
+
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret" "gcs-secret-key" {
+  project   = var.project_id
+  secret_id = "blue-owl-transfer-gcs-secret-key"
+
+  labels = {
+    environment = var.environment
+    function    = "blue-owl-transfer"
+    client      = "blue-owl"
+  }
+
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret" "aws-access-key" {
+  project   = var.project_id
+  secret_id = "blue-owl-transfer-aws-access-key"
+
+  labels = {
+    environment = var.environment
+    function    = "blue-owl-transfer"
+    client      = "blue-owl"
+  }
+
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret" "aws-secret-key" {
+  project   = var.project_id
+  secret_id = "blue-owl-transfer-aws-secret-key"
+
+  labels = {
+    environment = var.environment
+    function    = "blue-owl-transfer"
+    client      = "blue-owl"
+  }
+
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_iam_member" "gcs-access-key-iam" {
+  project   = var.project_id
+  secret_id = google_secret_manager_secret.gcs-access-key.id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${var.create_service_account ? google_service_account.runner[0].email : data.google_service_account.runner[0].email}"
+}
+
+resource "google_secret_manager_secret_iam_member" "gcs-secret-key-iam" {
+  project   = var.project_id
+  secret_id = google_secret_manager_secret.gcs-secret-key.id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${var.create_service_account ? google_service_account.runner[0].email : data.google_service_account.runner[0].email}"
+}
+
+resource "google_secret_manager_secret_iam_member" "aws-access-key-iam" {
+  project   = var.project_id
+  secret_id = google_secret_manager_secret.aws-access-key.id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${var.create_service_account ? google_service_account.runner[0].email : data.google_service_account.runner[0].email}"
+}
+
+resource "google_secret_manager_secret_iam_member" "aws-secret-key-iam" {
+  project   = var.project_id
+  secret_id = google_secret_manager_secret.aws-secret-key.id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${var.create_service_account ? google_service_account.runner[0].email : data.google_service_account.runner[0].email}"
+}
+
+resource "google_secret_manager_secret_version" "gcs-access-key" {
+  secret      = google_secret_manager_secret.gcs-access-key.id
+  secret_data = google_storage_hmac_key.gcp_acces_key.access_id
+}
+
+resource "google_secret_manager_secret_version" "gcs-secret-key" {
+  secret      = google_secret_manager_secret.gcs-secret-key.id
+  secret_data = google_storage_hmac_key.gcp_acces_key.secret
+}
+
+resource "google_service_account" "runner" {
+  count        = var.create_service_account ? 1 : 0
+  project      = var.project_id
+  account_id   = var.service_account_id
+  display_name = var.service_account_display_name
+  description  = var.service_account_description
+}
+
+data "google_service_account" "runner" {
+  count      = var.create_service_account ? 0 : 1
+  project    = local.service_account_project_id
+  account_id = var.service_account_id
+}
+
+resource "google_storage_hmac_key" "gcp_acces_key" {
+  project               = local.service_account_project_id
+  service_account_email = var.create_service_account ? google_service_account.runner[0].email : data.google_service_account.runner[0].email
+}
+
+resource "google_storage_bucket_iam_member" "gcs_buckets" {
+  for_each = toset(var.buckets)
+  bucket   = each.key
+  role     = "roles/storage.objectAdmin"
+  member   = "serviceAccount:${var.create_service_account ? google_service_account.runner[0].email : data.google_service_account.runner[0].email}"
+}
+
+resource "google_project_iam_member" "logs-writer" {
+  project = var.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${var.create_service_account ? google_service_account.runner[0].email : data.google_service_account.runner[0].email}"
+}
+
+resource "google_project_iam_member" "function-invoker" {
+  project = var.project_id
+  role    = "roles/cloudfunctions.invoker"
+  member  = "serviceAccount:${var.create_service_account ? google_service_account.runner[0].email : data.google_service_account.runner[0].email}"
+}
+
+resource "google_pubsub_topic" "topic" {
+  project = var.project_id
+  name    = "blue-owl-transfer-${var.environment}"
+
+  labels = {
+    environment = var.environment
+    function    = "blue-owl-transfer"
+    client      = "blue-owl"
+  }
+}
+
+resource "google_pubsub_subscription" "subscription" {
+  project = var.project_id
+  name    = "blue-owl-transfer-${var.environment}"
+  topic   = google_pubsub_topic.topic.name
+  labels = {
+    environment = var.environment
+    function    = "blue-owl-transfer"
+    client      = "blue-owl"
+  }
+}
+
+resource "google_pubsub_topic_iam_member" "publisher-iam" {
+  project = var.project_id
+
+  topic  = google_pubsub_topic.topic.name
+  role   = "roles/pubsub.publisher"
+  member = var.create_service_account ? google_service_account.runner[0].member : data.google_service_account.runner[0].member
+}
+
+resource "google_pubsub_subscription_iam_member" "subscriber-iam" {
+  project = var.project_id
+
+  subscription = google_pubsub_subscription.subscription.name
+  role         = "roles/pubsub.subscriber"
+  member       = var.create_service_account ? google_service_account.runner[0].member : data.google_service_account.runner[0].member
+}
+
+resource "google_service_account_iam_member" "workload-identity" {
+  count              = var.k8s_cluster_project_id == "" ? 0 : 1
+  member             = "serviceAccount:${var.k8s_cluster_project_id}.svc.id.goog[${var.k8s_namespace}/${var.k8s_service_account_name}]"
+  role               = "roles/iam.workloadIdentityUser"
+  service_account_id = var.create_service_account ? google_service_account.runner[0].name : data.google_service_account.runner[0].name
+}
---
+++ applications/blue-owl-transfer/variables.tf
@@ -0,0 +1,58 @@
+variable "environment" {
+  type        = string
+  description = "The environment the function will be deployed to"
+
+}
+
+variable "project_id" {
+  type        = string
+  description = "The project ID the function will be deployed to"
+}
+
+
+variable "create_service_account" {
+  type        = bool
+  description = "value to determine if a service account should be created"
+}
+
+variable "service_account_project_id" {
+  type        = string
+  description = "The project the service account is located in"
+  default     = ""
+}
+
+variable "service_account_id" {
+  type        = string
+  description = "The project the service account is located in"
+  default     = ""
+}
+
+variable "service_account_display_name" {
+  type        = string
+  description = "The project the service account is located in"
+  default     = ""
+}
+
+variable "service_account_description" {
+  type        = string
+  description = "The project the service account is located in"
+  default     = ""
+}
+
+variable "buckets" {
+  type        = list(string)
+  description = "The name of the bucket to be created"
+  default     = ["moove-aws-import"]
+}
+
+variable "k8s_cluster_project_id" {
+
+}
+
+variable "k8s_namespace" {
+
+}
+
+variable "k8s_service_account_name" {
+
+}
